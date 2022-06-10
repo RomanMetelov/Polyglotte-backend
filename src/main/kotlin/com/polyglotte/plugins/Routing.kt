e41d75a2e4bb095168d200042b4cfb938b8f225d
Roman Metelov <roman.metelov@pm.bet>
2022-06-10 15:46:48 +0300

    BIEN-1: pseudo json in response

--- src/main/kotlin/com/polyglotte/plugins/Routing.kt
+++ src/main/kotlin/com/polyglotte/plugins/Routing.kt
@@ -1,16 +1,14 @@
 package com.polyglotte.plugins
 
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondText("Hellyo World!")
+            call.respondText("{\"count\":20}")
         }
     }
 }
